dcb0bff2c1fa50f342c76a497a5fdbe85ca7fbc4
Kabira K <kabiraatmonallabs@devel0.my.domain>
2022-12-03 00:09:21 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # Introduction
 
-Originally, this repository was fork of [https://github.com/mewwts/addict](addict) by Mats Julian Olsen.
-Overtime, it has substatially diverged in functionality and maintainence code that it seemed to make
+Originally, this repository was a fork of [https://github.com/mewwts/addict](addict) by Mats Julian Olsen.
+Overtime, it has substatially diverged in functionality and codebase that it seemed to make
 sense to breakout as its own repository. 
 
-addict provides an alternative and succient interface to manipulate a dictionary. This is especially
+The original addict:  provides an alternative and succient interface to manipulate a dictionary. This is especially
 useful when dealing with heavily nested dictionaries. As example (taken from https://github.com/mewwts/addict)
 a dictionary created using standard python dictionary interface looks as follows:
-``python
+```python
 body = {
     'query': {
         'filtered': {
@@ -32,13 +32,15 @@ body.query.filtered.filter.term.created_by = 'Mats'
 
 This repo builds on original addict and adds contraptions to track changes in the addict. 
 This features comes in quite handy for reactive webapps where one has to respond 
-to all the changes made on the browser. 
+to all the changes made on the browser. Addict-tracking-changes is the underpinning
+data-structure in ofjustpy: a python based webframework build from [](justpy)
  
 
 
 
-## Usage:
+## Usage
 
+### Get and clear history
 ```python
 mydict = Dict()
 
@@ -49,10 +51,10 @@ for _ in mydict.get_changed_history():
 ```
 The output
 ```
-.a.b.c
+/a/b/c
 ```
 
-Use clear_changed_history to clear all changed history.
+Use clear_changed_history method to clear all changed history.
 
 ```
 mydict.clear_changed_history()
@@ -61,21 +63,73 @@ for _ in mydict.get_changed_history():
 ```
 outputs empty. 
 
-## Key Points
+### Freeze/unfreeze 
+Once a dict is frozen, adding new keys will raise KeyError. 
+Although modification to existing keys is allowed 
 
-1. Only tracks changes in values. Field additions and deletions are not being tracked. 
-2. Every field keeps a `__tracker` variable to keep track of all its members that have changed. 
+To freeze:
+```
+mydict.freeze()
+```
+
+To unfreeze:
+```
+mydict.unfreeze()
+```
+
+### pickling
+History is lost during pickling dump and load operation. 
+To enable tracking after pickle.load, use set_tracker operation.
 
 
-# TODO
-1. History not valid after
-pickle.load
-2. building dict from another dict
-following expression wont' work
+### Handling dict and list as values of keys
+A py-dict is treated as any other opaque value object. 
+Hence,
+```python
+mydict = Dict()
+
+mydict.a.b.c = {'kk': 1}
+mydict.a.b.e = {'dd': 1}
+
+for _ in mydict.get_changed_history():
+    print(_) 
+```
+will print paths
+```
+/a/b/c
+/a/b/e
+```
+and not 
+```
+/a/b/cc/kk
+/a/b/e/dd
+```
+
+List values on the other hand are exposed. Addict will walk within the list recursively,
+an report all the list location. So, for following Dict with list values:
+```
+        trackerprop.a.b = [1, [1]]
+        trackerprop.a.c = [2, [2, [3]]]
+```
+get_changed_history will report following paths:
+```
+            "/a/b/0",
+            "/a/b/1/0",
+            "/a/c/0",
+            "/a/c/1/0",
+            "/a/c/1/1/0",
+```
+
+
+
+## Caveats
+1. Only tracks  field additions.  Deletions and updates are not tracked. 
+2. freeze doesn't guards against deletions
+3. 
+4. building dict from another dict following expression wont' work
 ```python
 cjs_cfg = Dict(x, track_changes=True)
 ```
-
 instead use
 ```python 
 cjs_cfg = Dict(track_changes = True)
@@ -85,3 +139,5 @@ for _ in oj.dictWalker(x):
     oj.dnew(cjs_cfg, _[0], _[1])
 
 ```
+
+
--- src/addict/addict.py
+++ src/addict/addict.py
@@ -2,6 +2,7 @@ import copy
 
 # don't want namedtuple to dictionified
 import logging
+
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
@@ -11,7 +12,7 @@ def isnamedtupleinstance(x):
     b = t.__bases__
     if len(b) != 1 or b[0] != tuple:
         return False
-    f = getattr(t, '_fields', None)
+    f = getattr(t, "_fields", None)
     if not isinstance(f, tuple):
         return False
     return all(type(n) == str for n in f)
@@ -20,10 +21,13 @@ def isnamedtupleinstance(x):
 def get_changed_history_list(arritems, tprefix="", path_guards=None):
     for idx, aitem in enumerate(arritems):
         if isinstance(aitem, Dict):
-            yield from aitem.get_changed_history(tprefix+f"/{idx}", path_guards = path_guards
-                                                 )
+            yield from aitem.get_changed_history(
+                tprefix + f"/{idx}", path_guards=path_guards
+            )
         elif isinstance(aitem, list):
-            yield from get_changed_history_list(aitem, tprefix+f"/{idx}", path_guards = path_guards)
+            yield from get_changed_history_list(
+                aitem, tprefix + f"/{idx}", path_guards=path_guards
+            )
         else:
             # list are by default tracked for changes
             yield tprefix + f"/{idx}"
@@ -41,13 +45,14 @@ def clear_changed_history_list(arritems):
 
 class Dict(dict):
     def __init__(__self, *args, **kwargs):
-        object.__setattr__(__self, '__parent', kwargs.pop('__parent', None))
-        object.__setattr__(__self, '__key', kwargs.pop('__key', None))
-        object.__setattr__(__self, '__frozen', False)
-        object.__setattr__(__self, '__track_changes',
-                           kwargs.pop('track_changes', False))
-        if object.__getattribute__(__self, '__track_changes'):
-            object.__setattr__(__self, '__tracker', set())
+        object.__setattr__(__self, "__parent", kwargs.pop("__parent", None))
+        object.__setattr__(__self, "__key", kwargs.pop("__key", None))
+        object.__setattr__(__self, "__frozen", False)
+        object.__setattr__(
+            __self, "__track_changes", kwargs.pop("track_changes", False)
+        )
+        if object.__getattribute__(__self, "__track_changes"):
+            object.__setattr__(__self, "__tracker", set())
         for arg in args:
             if not arg:
                 continue
@@ -65,47 +70,44 @@ class Dict(dict):
 
     def __setattr__(self, name, value):
         if hasattr(self.__class__, name):
-            raise AttributeError("'Dict' object attribute "
-                                 "'{0}' is read-only".format(name))
+            raise AttributeError(
+                "'Dict' object attribute " "'{0}' is read-only".format(name)
+            )
         else:
             self[name] = value
 
     def __setitem__(self, name, value):
-        isFrozen = (hasattr(self, '__frozen') and
-                    object.__getattribute__(self, '__frozen'))
+        isFrozen = hasattr(self, "__frozen") and object.__getattribute__(
+            self, "__frozen"
+        )
         if isFrozen and name not in super(Dict, self).keys():
             raise KeyError(name)
 
         # import traceback
         # import sys
         # traceback.print_stack(file=sys.stdout)
-        # pickle.load calls setitem with out calling init 
+        # pickle.load calls setitem with out calling init
         try:
-            if object.__getattribute__(self, '__track_changes'):
-                object.__getattribute__(self, '__tracker').add((name))
+            if object.__getattribute__(self, "__track_changes"):
+                object.__getattribute__(self, "__tracker").add((name))
         except:
             pass
-        
+
         super(Dict, self).__setitem__(name, value)
         try:
-            p = object.__getattribute__(self, '__parent')
-            key = object.__getattribute__(self, '__key')
+            p = object.__getattribute__(self, "__parent")
+            key = object.__getattribute__(self, "__key")
         except AttributeError:
             p = None
             key = None
         if p is not None:
             p[key] = self
-            object.__delattr__(self, '__parent')
-            object.__delattr__(self, '__key')
+            object.__delattr__(self, "__parent")
+            object.__delattr__(self, "__key")
 
     def __add__(self, other):
-        if not self.keys():
-            return other
-        else:
-            self_type = type(self).__name__
-            other_type = type(other).__name__
-            msg = "unsupported operand type(s) for +: '{}' and '{}'"
-            raise TypeError(msg.format(self_type, other_type))
+
+        raise NotImplementedError("add operation not supported")
 
     @classmethod
     def _hook(cls, item):
@@ -119,9 +121,13 @@ class Dict(dict):
         return self.__getitem__(item)
 
     def __missing__(self, name):
-        if object.__getattribute__(self, '__frozen'):
+        if object.__getattribute__(self, "__frozen"):
             raise KeyError(name)
-        return self.__class__(__parent=self, __key=name, track_changes=object.__getattribute__(self, '__track_changes'))
+        return self.__class__(
+            __parent=self,
+            __key=name,
+            track_changes=object.__getattribute__(self, "__track_changes"),
+        )
 
     def __delattr__(self, name):
         del self[name]
@@ -133,8 +139,9 @@ class Dict(dict):
                 base[key] = value.to_dict()
             elif isinstance(value, (list, tuple)):
                 base[key] = type(value)(
-                    item.to_dict() if isinstance(item, type(self)) else
-                    item for item in value)
+                    item.to_dict() if isinstance(item, type(self)) else item
+                    for item in value
+                )
             else:
                 base[key] = value
         return base
@@ -160,9 +167,11 @@ class Dict(dict):
             other.update(args[0])
         other.update(kwargs)
         for k, v in other.items():
-            if ((k not in self) or
-                (not isinstance(self[k], dict)) or
-                    (not isinstance(v, dict))):
+            if (
+                (k not in self)
+                or (not isinstance(self[k], dict))
+                or (not isinstance(v, dict))
+            ):
                 self[k] = v
             else:
                 self[k].update(v)
@@ -202,7 +211,7 @@ class Dict(dict):
             return default
 
     def freeze(self, shouldFreeze=True):
-        object.__setattr__(self, '__frozen', shouldFreeze)
+        object.__setattr__(self, "__frozen", shouldFreeze)
         for key, val in self.items():
             if isinstance(val, Dict):
                 val.freeze(shouldFreeze)
@@ -210,21 +219,25 @@ class Dict(dict):
     def unfreeze(self):
         self.freeze(False)
 
-    def get_changed_history(self, prefix="", path_guards = None):
+    def get_changed_history(self, prefix="", path_guards=None):
         if super().__getattribute__("__track_changes") == False:
             return
 
         for key, value in self.items():
             if isinstance(value, type(self)):
                 try:
-                    if not path_guards or prefix+"/" + str(key) not in path_guards:
-                        yield from value.get_changed_history(prefix+"/" + str(key), path_guards=path_guards)
+                    if not path_guards or prefix + "/" + str(key) not in path_guards:
+                        yield from value.get_changed_history(
+                            prefix + "/" + str(key), path_guards=path_guards
+                        )
                 except Exception as e:
                     logger.debug(f"concat error {self.items} {self}")
                     logger.debug(f"concat error {self}")
                     raise e
             elif isinstance(value, list):
-                yield from get_changed_history_list(value, prefix+"/" + str(key), path_guards=path_guards)
+                yield from get_changed_history_list(
+                    value, prefix + "/" + str(key), path_guards=path_guards
+                )
             else:
                 if key in super().__getattribute__("__tracker"):
                     yield prefix + "/" + key
@@ -253,12 +266,10 @@ class Dict(dict):
         for key, value in self.items():
             if isinstance(value, type(self)):
                 value.set_tracker(self)
-        object.__setattr__(self, '__frozen',
-                           False)
-        object.__setattr__(self, '__track_changes',
-                           track_changes)
+        object.__setattr__(self, "__frozen", False)
+        object.__setattr__(self, "__track_changes", track_changes)
         if track_changes:
-            object.__setattr__(self, '__tracker', set())
+            object.__setattr__(self, "__tracker", set())
 
 
 def walker(adict, ppath="", guards=None):
